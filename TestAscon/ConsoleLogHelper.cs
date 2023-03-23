ba593f19a827e93f31bd4ea6c5d6dff46700599c
Daniil <timofeev742@gmail.com>
2023-03-23 23:09:23 +0400

    Добавьте файлы проекта.

---
+++ TestAscon/ConsoleLogHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAscon
+{
+    internal static class ConsoleLogHelper
+    {
+        public static void StartSerializationMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Информация: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Начало сериализации. " + DateTime.Now);
+        }
+
+        public static void EndSerializationMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Информация: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Завершение десериализации. " + DateTime.Now);
+        }
+
+        public static void StartDeserializationMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Информация: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Начало десериализации. " + DateTime.Now);
+        }
+
+        public static void EndDeserializationMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Информация: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Завершение десериализации. " + DateTime.Now);
+        }
+
+        public static void StartDeleteDublicatesMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Информация: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Начало удаления дубликатов. " + DateTime.Now);
+        }
+
+        public static void EndDeleteDublicatesMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Информация: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Удаление дубликатов завершено. " + DateTime.Now);
+        }
+
+        public static void FileNotExistErrorMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Ошибка: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Файл не найден. " + DateTime.Now);
+        }
+
+        public static void FileIsEmptyErrorMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Ошибка: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Файл пуст. " + DateTime.Now);
+        }
+
+        public static void StartUpdateFileMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Информация: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Перезапись файла начата. " + DateTime.Now);
+        }
+
+        public static void EndUpdateFileMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Информация: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Перезапись файла закончена. " + DateTime.Now);
+        }
+    }
+}
---
+++ TestAscon/Program.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using TestAscon;
+
+var objectsList = GetObjectsList<SomeClass>("ShortSummary.json");
+var listWithoutDublicates = DeleteSame(objectsList);
+var json = ConvertToJson(listWithoutDublicates);
+UpdateJsonFile(json);
+Console.ReadKey();
+
+// Получает список объектов на основе переданного json.
+List<T> GetObjectsList<T>(string name) where T : class
+{
+    ConsoleLogHelper.StartDeserializationMessage();
+
+    var path = Path.GetFullPath(name);
+
+    if (!File.Exists(path))
+    {
+        ConsoleLogHelper.FileNotExistErrorMessage();
+
+        throw new Exception();
+    }
+
+    var jsonString = File.ReadAllText(path);
+    var someObjects = JsonConvert.DeserializeObject<List<T>>(jsonString);
+
+    if (someObjects is null)
+    {
+        ConsoleLogHelper.FileIsEmptyErrorMessage();
+
+        throw new Exception();
+    }
+
+    ConsoleLogHelper.EndDeserializationMessage();
+
+    return someObjects;
+}
+
+// Удаляет дубликаты из переданного списка объектов.
+List<SomeClass> DeleteSame(List<SomeClass> list)
+{
+    ConsoleLogHelper.StartDeleteDublicatesMessage();
+
+    var result = new List<SomeClass>();
+
+    foreach (var i in list)
+    {
+        if (!result.Exists(x => (x.rec_id == i.rec_id) &&
+                                (x.timestamp == i.timestamp)))
+        {
+            result.Add(i);
+        }
+    }
+
+    ConsoleLogHelper.EndDeleteDublicatesMessage();
+
+    return result;
+}
+
+// Преобразует список объектов в строку json.
+string ConvertToJson<T>(List<T> list) where T : class
+{
+    ConsoleLogHelper.StartSerializationMessage();
+
+    string json = JsonConvert.SerializeObject(list);
+
+    ConsoleLogHelper.EndSerializationMessage();
+
+    return json;
+}
+
+// Обновляет json файл без дублирующих объектов.
+async void UpdateJsonFile(string json)
+{
+    var path = Path.GetFullPath("ShortSummary.json");
+
+    if (!File.Exists(path))
+    {
+        ConsoleLogHelper.FileNotExistErrorMessage();
+
+        throw new Exception();
+    }
+
+    ConsoleLogHelper.StartUpdateFileMessage();
+
+    using (var writer = new StreamWriter(path, false))
+    {
+        await writer.WriteLineAsync(json);
+    }
+
+    ConsoleLogHelper.EndUpdateFileMessage();
+}
